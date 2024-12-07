96ae723d9aa0b94a87aca0b455d4507db3a6cdcb
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-07 19:59:07 -0300

    Update createTables.js

--- src/config/createTables.js
+++ src/config/createTables.js
@@ -1,9 +1,7 @@
-import { sequelize } from "./database.js"; // Importa a instância do Sequelize corretamente
-
+import { sequelize } from "./database.js";
 const createTables = async () => {
   try {
-    // Sincroniza as tabelas registradas no Sequelize
-    await sequelize.sync({ force: false }); // `force: true` recria as tabelas, mas apaga dados
+    await sequelize.sync({ force: false });
     console.log("Tabelas criadas com sucesso!");
   } catch (error) {
     console.error("Erro ao criar tabelas:", error);
