a88af6944e2c7b82ffcd911c4eaf25eddff3640c
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-07 20:06:44 -0300

    Add files via upload

---
+++ src/controllers/speciesController.js
@@ -0,0 +1,26 @@
+import { Species } from "../config/database.js";
+
+export const getSpeciesById = async (req,res) =>{
+    const { id } = req.params;
+
+    try{
+        const species = await Species.findByPk(id);
+        if(!species){
+            return res.status(404).json({error: "Especie não econtrada."});
+        }
+        res.status(200).json(species);
+    } catch (error){
+        res.status(500).json({ message: "Erro: ", error});
+    }
+};
+
+export const createSpecies = async(req,res) =>{
+    try{
+        const { nome, nomePopular, bioma, habitat, altura, diametro, longevidade, Urlimage} = req.body;
+
+        const newSpecies = await Species.create({ nome, nomePopular, bioma, habitat, altura, diametro, longevidade, Urlimage });
+        res.status(200).json(newSpecies);
+    } catch (error){
+        res.status(500).json({ message: "Erro ", error});
+    }
+}
