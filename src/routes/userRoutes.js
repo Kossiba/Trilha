8f28cf9ccad1136e3c7cfdfa1d07c354163bfc6d
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-09 09:48:10 -0300

    Update userRoutes.js
    Adicionando login

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getAllUsers, createUser } from "../controllers/userController.js";
+import { getAllUsers, createUser, login } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/", getAllUsers);
 router.post("/", createUser); 
+router.post("/login", login);
+
 export default router;
