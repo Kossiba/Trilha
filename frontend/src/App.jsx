a1f827cc761c1db86f3d1556eac82d86d8f6e745
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-10 15:22:51 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,33 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Camera from "./pages/Camera";
+import { syncUsersFromBackend } from "./dbStatic/sync";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [isSyncing, setIsSyncing] = useState(false);
+
+  useEffect(() => {
+    async function synchronizeData() {
+      if (navigator.onLine) {
+        setIsSyncing(true);
+        await syncUsersFromBackend();
+        setIsSyncing(false);
+      }
+    }
+  
+    synchronizeData();
+  
+    window.addEventListener("online", synchronizeData);
+  
+    return () => {
+      window.removeEventListener("online", synchronizeData);
+    };
+  }, []);
+
   return (
     <Router>
+      {isSyncing && <div className="sync-message">Sincronizando dados...</div>}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
