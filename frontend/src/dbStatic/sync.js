5e562a63f3e28ec2c42d4b4195f60d1e9066f8c3
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-11 11:09:34 -0300

    Update sync.js

--- frontend/src/dbStatic/sync.js
+++ frontend/src/dbStatic/sync.js
@@ -1,4 +1,4 @@
-import { addAllUsers } from "./offline-db.js";
+import { addAllUsers, getAllSpecies } from "./offline-db.js";
 
 export async function syncUsersFromBackend() {
   try {
@@ -9,8 +9,22 @@ export async function syncUsersFromBackend() {
 
     const users = await response.json();
     await addAllUsers(users);
-    console.log("Sincronização de usuários concluída.", users);
   } catch (error) {
     console.error("Erro ao sincronizar usuários:", error);
+  }
+}
+
+export async function syncSpeciesFromBackend() {
+  try {
+    const response = await fetch("https://trilha-2vfh.onrender.com/species");
+    if (!response.ok) {
+      throw new Error("Erro ao buscar Species do backend");
+    }
+
+    const species = await response.json();
+    await getAllSpecies(species);
+    console.log("Sincronização de Species concluída.", species);
+  } catch (error) {
+    console.error("Erro ao sincronizar Species:", error);
   }
 }
