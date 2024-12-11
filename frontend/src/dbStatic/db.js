e06d1fd048ec957d325698c375725f36f7e275cf
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-11 17:10:32 -0300

    Update db.js

--- frontend/src/dbStatic/db.js
+++ frontend/src/dbStatic/db.js
@@ -3,14 +3,20 @@ import { openDB } from "idb";
 const dbPromise = openDB("app-db", 1, {
   upgrade(db) {
     if (!db.objectStoreNames.contains("users")) {
-      const userStore = db.createObjectStore("users", { keyPath: "id", autoIncrement: true });
+      const userStore = db.createObjectStore("users", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
       userStore.createIndex("name", "name", { unique: true });
       userStore.createIndex("email", "email", { unique: true });
       userStore.createIndex("password", "password", { unique: false });
     }
 
     if (!db.objectStoreNames.contains("species")) {
-      const speciesStore = db.createObjectStore("species", { keyPath: "id", autoIncrement: true });
+      const speciesStore = db.createObjectStore("species", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
       speciesStore.createIndex("nome", "nome", { unique: true });
       speciesStore.createIndex("nomePopular", "nomePopular", { unique: false });
       speciesStore.createIndex("bioma", "bioma", { unique: false });
