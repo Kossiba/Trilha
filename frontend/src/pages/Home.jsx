6ea01e8af2572de6359455f6732a7354579fdb15
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-15 12:17:10 -0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,77 @@
-import "../styles/Home.css";
-import imgHome from "../assets/imgHome.png";
-import imgLogo from "../assets/imgLogoUTFPR.png";
-import { useNavigate } from "react-router-dom";
+import "../styles/Menu.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapLocationDot, faQrcode } from "@fortawesome/free-solid-svg-icons";
+import { faFile, faUser } from "@fortawesome/free-regular-svg-icons";
+import { useNavigate} from "react-router-dom";
+import PropTypes from "prop-types";
+import imgLogin from "../assets/imgLogin.png";
 
-const Home = () => {
-  const navigate = useNavigate();
-
-  const handleMenuClick = () => {
-    navigate("/Menu");
-  };
-
+const Card = ({ icon, rotation, title, description, onClick }) => {
   return (
-    <div className="home-container">
-      <img src={imgHome} className="img-home"></img>
-      <a className="text-home">Bem-vindo a trilha ecológica</a>
-      <a className="text-home01">da UTFPR</a>
-      <button className="button-home" onClick={handleMenuClick}>
-        CONTINUAR
-      </button>
-      <img src={imgLogo} className="img-logo"></img>
+    <div className="card-div" onClick={onClick}>
+      <div className="card-icon">
+        <FontAwesomeIcon icon={icon} rotation={rotation} size="2x" />
+      </div>
+      <h3 className="card-title">{title}</h3>
+      <p className="card-description">{description}</p>
     </div>
   );
 };
 
-export default Home;
+Card.propTypes = {
+  icon: PropTypes.object.isRequired,
+  rotation: PropTypes.number,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+const Menu = () => {
+  const navigate = useNavigate();
+  const handleCameraClick = () => {
+    navigate("/Camera");
+  };
+
+  const handleLoginClick = () => {
+    navigate("/Login");
+  };
+
+  return (
+    <div className="menu-container">
+      <div className="menucards-div-menu">
+        <div className="cards-div">
+          <Card
+            icon={faMapLocationDot}
+            title="MAPA"
+            description="Explore a trilha"
+            onClick={handleCameraClick}
+          />
+          <Card
+            icon={faQrcode}
+            title="LER QRCODE"
+            description="Aprenda sobre fauna e flora"
+            onClick={handleCameraClick}
+          />
+        </div>
+        <div className="cards-div">
+          <Card
+            icon={faFile}
+            rotation={180}
+            title="CARDS"
+            description="Visualize seus cards"
+            onClick={handleCameraClick}
+          />
+          <Card
+            icon={faUser}
+            title="ENTRAR"
+            description="Realize login"
+            onClick={handleLoginClick}
+          />
+        </div>
+      </div>
+      <img src={imgLogin} className="img-menu" alt="Login" />
+    </div>
+  );
+};
+
+export default Menu;
