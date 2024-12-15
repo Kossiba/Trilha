098f54c1d3580bac30de54bfb923bd3974992e6c
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-15 12:15:23 -0300

    Update CardDetails.jsx

--- frontend/src/pages/CardDetails.jsx
+++ frontend/src/pages/CardDetails.jsx
@@ -1,45 +1,56 @@
-import { useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { getSpeciesById } from "../dbStatic/offline-db";
+//import { useLocation } from "react-router-dom";
+//import { useEffect, useState } from "react";
+import imgTest from "../assets/imgHome.png";
+//import { getSpeciesById } from "../dbStatic/offline-db";
 import "../styles/CardDetails.css";
 
 const CardDetails = () => {
+  // Código dinâmico comentado para uso estático
+  /*
   const location = useLocation();
   const { qrCodeData } = location.state || {};
   const [speciesDetails, setSpeciesDetails] = useState(null);
-
+  console.log(qrCodeData);
   useEffect(() => {
-    const fetchData = async () => {
-      if (qrCodeData) {
-        const data = await getSpeciesById(qrCodeData);
-        setSpeciesDetails(data);
+    const fetchSpeciesDetails = async () => {
+      try {
+        const response = await fetch(
+          "https://trilha-2vfh.onrender.com/species/${qrCodeData}"
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          setSpeciesDetails(data);
+        } else {
+          console.error("Failed to fetch species details");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
     };
 
-    fetchData();
+    fetchSpeciesDetails();
   }, [qrCodeData]);
+  */
 
+  // Conteúdo estático
   return (
     <div className="card-details-container">
-      <div className="view-card-details">
-        {speciesDetails ? (
-          <>
-            <img
-              src={speciesDetails.Urlimage || "#"}
-              alt="Detalhe do Card"
-              className="card-details-img"
-            />
-            <a>Nome: {speciesDetails.nome}</a>
-            <a>Nome popular: {speciesDetails.nomePopular}</a>
-            <a>Bioma: {speciesDetails.bioma}</a>
-            <a>Habitat: {speciesDetails.habitat}</a>
-            <a>Altura: {speciesDetails.altura}</a>
-            <a>Diâmetro: {speciesDetails.diametro}</a>
-            <a>Longevidade: {speciesDetails.longevidade}</a>
-          </>
-        ) : (
-          <p>Carregando dados...</p>
-        )}
+      <div className="view-carddetails">
+        <div className="view-top-carddetaiils"></div>
+        <img
+          src={imgTest}
+          alt="Detalhe do Card"
+          className="card-details-img"
+        />
+        <a>Nome: nomeTeste</a>
+        <a>Nome popular: nomePopularTeste</a>
+        <a>Bioma: biomaTeste</a>
+        <a>Habitat: habitatTeste</a>
+        <a>Altura: Não informada</a>
+        <a>Diâmetro: Não informado</a>
+        <a>Longevidade: Não informada</a>
+        <div className="view-bottom-carddetaiils"></div>
       </div>
     </div>
   );
