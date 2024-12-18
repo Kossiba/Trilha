c7b15029c6311b5d55d0726c9e2f25ab8452d744
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-18 10:13:54 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Menu from "./pages/Menu";
+
 import QRCodeScanner from "./pages/QRCodeScanner";
 import CardDetails from "./pages/CardDetails";
-import SignUp from "./pages/SignUp";
 import TelaInicial from "./pages/TelaInicial";
 import Entrar from "./pages/Entrar";
 import Info from "./pages/TrilhaEcologica";
@@ -43,14 +40,10 @@ const App = () => {
     <Router>
       {isSyncing && <div className="sync-message">Sincronizando dados...</div>}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tela-inicial" element={<TelaInicial />} />
+        <Route path="/" element={<TelaInicial />} />
         <Route path="/info" element={<Info />} />
         <Route path="/entrar" element={<Entrar />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
         <Route path="/nova-senha" element={<ChangePassword />} />
-        <Route path="/menu" element={<Menu />} />
         <Route path="/camera" element={<QRCodeScanner />} />
         <Route path="/card-details" element={<CardDetails />} />
         <Route path="/card-list" element={<CardList />} />
