522439d30a84fa1fdc4bf639cf286b180c7fabce
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-18 17:29:43 -0300

    Update CardList.jsx

--- frontend/src/pages/CardList.jsx
+++ frontend/src/pages/CardList.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import NavBar from "../components/NavBar";
+import { fetchSpeciesFromCache } from "../dbStatic/offline-db";
 import { useNavigate } from "react-router-dom";
 
 const CardList = () => {
@@ -13,15 +14,30 @@ const CardList = () => {
 
   const fetchSpecies = async () => {
     try {
-      const response = await fetch("https://trilha-2vfh.onrender.com/species");
-      if (!response.ok) {
-        throw new Error("Erro ao buscar as espécies");
+      if (navigator.onLine) {
+        console.log("Tentando buscar os dados online...");
+        const response = await fetch("https://trilha-2vfh.onrender.com/species");
+        if (response.ok) {
+          const data = await response.json();
+          setSpecies(data);
+          setLoading(false);
+        } else {
+          console.error("Falha ao buscar dados online. Tentando offline...");
+          throw new Error("Falha ao buscar online");
+        }
+      } else {
+        console.log("Sem conexão. Tentando buscar offline...");
+        const result = await fetchSpeciesFromCache();
+        if (result) {
+          setSpecies(result);
+          setLoading(false);
+        } else {
+          throw new Error("Não foi possível carregar os detalhes da espécie offline.");
+        }
       }
-      const data = await response.json();
-      setSpecies(data);
-      setLoading(false);
     } catch (err) {
-      setError(err.message);
+      setError("Erro ao carregar os dados da espécie.");
+      console.error("Erro ao buscar os dados:", err);
       setLoading(false);
     }
   };
@@ -33,11 +49,9 @@ const CardList = () => {
   if (loading) return <p>Carregando...</p>;
   if (error) return <p>Erro: {error}</p>;
 
-  
   const handleCardClick = (id) => {
     navigate("/card-details", { state: { speciesId: id } });
   };
-
 
   return (
     <div className="container-cardlist">
