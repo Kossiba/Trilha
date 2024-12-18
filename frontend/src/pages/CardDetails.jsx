709ae2e5e3480c9f639199f9b0deb5ff76ca9c34
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-18 10:16:02 -0300

    Update CardDetails.jsx

--- frontend/src/pages/CardDetails.jsx
+++ frontend/src/pages/CardDetails.jsx
@@ -1,20 +1,32 @@
-import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
-import imgTest from "../assets/imgHome.png";
-import { getSpeciesById } from "../dbStatic/offline-db";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faShareNodes } from "@fortawesome/free-solid-svg-icons";
+//import { getSpeciesById } from "../dbStatic/offline-db";
 import "../styles/CardDetails.css";
+import { useNavigate, useLocation } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 const CardDetails = () => {
+  const navigate = useNavigate();
   const location = useLocation();
-  const { qrCodeData } = location.state || {};
+  const { qrCodeData, speciesId } = location.state || {};
   const [speciesDetails, setSpeciesDetails] = useState(null);
-  console.log(qrCodeData);
+
+  const handleTelaInicialClick = () => {
+    navigate("/tela-inicial");
+  };
+
   useEffect(() => {
+    const id = qrCodeData || speciesId;
+    if (!id) {
+      console.error("ID não fornecido. Redirecionando para a tela inicial.");
+      navigate("/tela-inicial");
+      return;
+    }
+
     const fetchSpeciesDetails = async () => {
       try {
-        const response = await fetch(
-          "https://trilha-2vfh.onrender.com/species/${qrCodeData}"
-        );
+        const response = await fetch(`http://localhost:3000/species/${id}`);
 
         if (response.ok) {
           const data = await response.json();
@@ -28,26 +40,49 @@ const CardDetails = () => {
     };
 
     fetchSpeciesDetails();
-  }, [qrCodeData]);
+  }, [qrCodeData, speciesId, navigate]);
 
-  // Conteúdo estático
+  if (!speciesDetails) {
+    return <p>Carregando detalhes...</p>;
+  }
+
   return (
     <div className="card-details-container">
-      <div className="view-carddetails">
-        <div className="view-top-carddetaiils"></div>
-        <img
-          src={imgTest}
-          alt="Detalhe do Card"
-          className="card-details-img"
+      <div className="header-carddetails">
+        <FontAwesomeIcon
+          icon={faArrowLeft}
+          style={{
+            color: "#0C9762",
+            width: "2.02vh",
+            height: "2.02vh",
+            marginLeft: "3.91vh",
+          }}
+          onClick={handleTelaInicialClick}
         />
-        <a>Nome: nomeTeste</a>
-        <a>Nome popular: nomePopularTeste</a>
-        <a>Bioma: biomaTeste</a>
-        <a>Habitat: habitatTeste</a>
-        <a>Altura: Não informada</a>
-        <a>Diâmetro: Não informado</a>
-        <a>Longevidade: Não informada</a>
-        <div className="view-bottom-carddetaiils"></div>
+        <p className="tittle-carddetails">Detalhes da espécie</p>
+        <FontAwesomeIcon
+          icon={faShareNodes}
+          style={{
+            color: "#0C9762",
+            width: "3vh",
+            height: "3vh",
+            marginRight: "4vh",
+          }}
+        />
+      </div>
+      <img src={speciesDetails.imgURL || "#"} className="img-carddetails" />
+      <div className="div-text-carddetails">
+        <p className="subttile-carddetails">
+          {speciesDetails.nomepopular} ({speciesDetails.nomecientifico})
+        </p>
+        <p className="text-carddetails">
+          {speciesDetails.descricao}
+          <p className="subttile-carddetails">Características</p>
+          <p className="text-carddetails">{speciesDetails.caracteristicas}</p>
+        </p>
+      </div>
+      <div className="div-navbar">
+        <NavBar />
       </div>
     </div>
   );
