9a47b8bae221b9ce5160be847e8ad2bd3f51ca59
Kossiba <150734694+Kossiba@users.noreply.github.com>
2024-12-18 09:49:48 -0300

    Update species.js

--- backend/models/species.js
+++ backend/models/species.js
@@ -1,46 +1,38 @@
 import { DataTypes } from "sequelize";
 
 export default (sequelize) => {
-  return sequelize.define("Species", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+  return sequelize.define(
+    "Species",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nomecientifico: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nomepopular: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      descricao: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      caracteristicas: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      imgURL: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    nome: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    nomePopular: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    bioma: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    habitat: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    altura: {
-      type: DataTypes.FLOAT,
-      allowNull: true,
-    },
-    diametro: {
-      type: DataTypes.FLOAT,
-      allowNull: true,
-    },
-    longevidade: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    Urlimage: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-  },{
-    tableName: "Species",
-    timestamps: true, 
-  });
+    {
+      tableName: "Species",
+      timestamps: true,
+    }
+  );
 };
