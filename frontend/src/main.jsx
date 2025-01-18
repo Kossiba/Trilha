3bb3b7faa3e5d34c4dce0015bb16c181d4415098
Kossiba <150734694+Kossiba@users.noreply.github.com>
2025-01-18 12:17:00 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,18 @@ import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// Registro do Service Worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then((registration) => {
+      console.log("Service Worker registrado com sucesso:", registration);
+    })
+    .catch((error) => {
+      console.error("Erro ao registrar o Service Worker:", error);
+    });
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
