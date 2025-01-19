f13dcb516518c3486003cc2530d4e62bc6a5bd26
Kossiba <150734694+Kossiba@users.noreply.github.com>
2025-01-19 11:35:11 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       manifest: {
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        short_name: 'App',
-        name: 'TrilhaUTFPR-DV',
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        short_name: "App",
+        name: "TrilhaUTFPR-DV",
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,webp,jpeg}'],
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,jpg,webp,jpeg}"],
+        globDirectory: "dist",
         additionalManifestEntries: [
           { url: "/", revision: null },
           { url: "/index.html", revision: null },
@@ -30,12 +31,12 @@ export default defineConfig({
         runtimeCaching: [
           {
             urlPattern: ({ request }) =>
-              request.destination === 'document' ||
-              request.destination === 'script' ||
-              request.destination === 'style',
-            handler: 'CacheFirst',
+              request.destination === "document" ||
+              request.destination === "script" ||
+              request.destination === "style",
+            handler: "CacheFirst",
             options: {
-              cacheName: 'vite-app-cache',
+              cacheName: "vite-app-cache",
               expiration: {
                 maxEntries: 50,
                 maxAgeSeconds: 30 * 24 * 60 * 60,
@@ -43,10 +44,10 @@ export default defineConfig({
             },
           },
           {
-            urlPattern: ({ request }) => request.destination === 'image',
-            handler: 'CacheFirst',
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
             options: {
-              cacheName: 'image-cache',
+              cacheName: "image-cache",
               expiration: {
                 maxEntries: 100,
                 maxAgeSeconds: 7 * 24 * 60 * 60,
@@ -54,6 +55,9 @@ export default defineConfig({
             },
           },
         ],
+      },
+      devOptions: {
+        enabled: true,
       },
     }),
   ],
