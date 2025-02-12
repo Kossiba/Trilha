e8d78b65a5f5b1c6dce877f9642febe9effd8a51
Kossiba <150734694+Kossiba@users.noreply.github.com>
2025-02-12 16:10:55 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import { NetworkProvider } from "./Context/NetworkProvider.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
-// Registro do Service Worker
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/service-worker.js")
-    .then((registration) => {
-      console.log("Service Worker registrado com sucesso:", registration);
-    })
-    .catch((error) => {
-      console.error("Erro ao registrar o Service Worker:", error);
-    });
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registrado com sucesso:", registration);
+      })
+      .catch((error) => {
+        console.error("Erro ao registrar o Service Worker:", error);
+      });
+  });
 }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <NetworkProvider>
+      <App />
+    </NetworkProvider>
   </StrictMode>
 );
